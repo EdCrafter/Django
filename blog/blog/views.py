687ee2a186640ec15929a934f7c97b0507d38d92
root <verdev4az@gmail.com>
2023-12-17 12:41:38 +0200

    28.11

--- blog/blog/views.py
+++ blog/blog/views.py
@@ -1,8 +1,8 @@
 # blog/views.py
 from django.views.generic import ListView, DetailView 
-
 from .models import Post
-
+from django.views.generic.edit import CreateView, UpdateView,DeleteView
+from django.urls import reverse_lazy
 
 class BlogListView(ListView):
     model = Post
@@ -11,3 +11,17 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView): # новое
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView): # новое изменение
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+class BlogUpdateView(UpdateView): # Новый класс
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+class BlogDeleteView(DeleteView): # Создание нового класса
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
--- blog/blog_project/urls.py
+++ blog/blog_project/urls.py
@@ -5,4 +5,5 @@ from django.urls import path, include # новое изменение
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')), # новое изменение
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
